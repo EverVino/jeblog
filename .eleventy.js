5a68e9d347015cb643453bbe9209f3882f9cd963
EverVino <evervino00@gmail.com>
2022-09-04 12:37:42 -0400

    modificaciones para mostrar ecuaciones matemáticas en los posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,44 @@
 const {DateTime} = require("luxon");
 
 module.exports = function(eleventyConfig) {
+    
+    // making copy of directories in the output public folder
     eleventyConfig.addPassthroughCopy("./src/style.css");
     eleventyConfig.addPassthroughCopy("./src/assets");
 
+   
+    let markdownIt = require("markdown-it");
+    let markdownItKatex = require("@iktakahiro/markdown-it-katex");
+    let options = {
+        html: true
+    };
+    let markdownLib = markdownIt(options)
+    markdownLib.use(markdownItKatex);
+    
+    eleventyConfig.setLibrary("md", markdownLib);
+    
+    
+    // Adding some math config 
+    // let markdownIt = require("markdown-it");
+    // let markdownItKatex = require("@iktakahiro/markdown-it-katex");
+    // let options = {
+    //     html: true,
+    //     breaks: true,
+    //     linkify: true,
+    //   };
+    // let md = new markdownIt(options);
+    // md.use(markdownItKatex);
+    
+    // let markdownLib = markdownIt(options).use(markdownItKatex);
+    //eleventyConfig.setLibrary("md", markdownLib);
+    
+    // eleventyConfig.addFilter("markdown", (content) => {
+        
+    //     return md.render(content);
+    //     });
+    
+
+    // Adding function for dates
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL)
     });
--- src/blog/new-article.md
+++ src/blog/new-article.md
@@ -1,6 +1,6 @@
 ---
-title: Nuevo Artículo
-description: Saludos un nuevo artículo
+title: Prueba de artículo
+description: post donde prueba le render de matemáticas
 author: Ever Vino
 date: 2022-08-31
 tags: post
@@ -8,4 +8,34 @@ image: /assets/blog/article-1.jpg
 imageAlt: nueva imagen
 ---
 
-Hola Como estan este es un nuevo artículo para mostar.
+Hola Como estan esta es una ecuación $\frac{1}{x+2}+x+1 = \sum$
+
+$$\sqrt{3x-1}+(1+x)^2=\frac{y+1}{x-1}$$
+
+un nuevo artículo para mostar.
+
+$$\begin{array}{c}
+
+\nabla \times \vec{\mathbf{B}} -\, \frac1c\, \frac{\partial\vec{\mathbf{E}}}{\partial t} &
+= \frac{4\pi}{c}\vec{\mathbf{j}}    \nabla \cdot \vec{\mathbf{E}} & = 4 \pi \rho \\
+
+\nabla \times \vec{\mathbf{E}}\, +\, \frac1c\, \frac{\partial\vec{\mathbf{B}}}{\partial t} & = \vec{\mathbf{0}} \\
+
+\nabla \cdot \vec{\mathbf{B}} & = 0
+
+\end{array}$$
+
+```python
+def factorial(n):
+    if n < 1:
+        return n
+    return n*factorial(n)
+```
+
+```js
+let f = function(){
+    let m = 23;
+    console.log(m);
+}
+
+```
